12c9db0d91d03e90a4a69fc25e880adbed9aff9c
Aneeq Zafar <zafaraay@gmail.com>
2025-06-15 21:11:03 +0500

    Project Phase 1 Completed (Client Side & Server Side Validation + Error Handling + Minor tweaks)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressErrors.js");
+const listingSchema = require("./schema.js");
 
 app.engine("ejs", ejsMate);
 
@@ -35,12 +38,25 @@ app.get("/", (req, res) => {
   res.send("Hello World! Root Here");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 //Index Route
-app.get("/listings", async (req, res) => {
-  let listings = await Listing.find({});
-  // console.log(listings);
-  res.render("listings/index.ejs", { listings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let listings = await Listing.find({});
+    // console.log(listings);
+    res.render("listings/index.ejs", { listings });
+  })
+);
 
 //New Route
 app.get("/listings/new", (req, res) => {
@@ -48,40 +64,71 @@ app.get("/listings/new", (req, res) => {
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  // console.log(listing);
-  res.render("listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    // console.log(listing);
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
-//Cretae Route
-app.post("/listings", async (req, res) => {
-  let listing = new Listing(req.body.listing);
-  await listing.save();
-  res.redirect("/listings");
-});
+//Create Route
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let listing = new Listing(req.body.listing);
+    await listing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  res.render("listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-  let listing = await Listing.findByIdAndUpdate(req.params.id, {
-    ...req.body.listing,
-  });
-  console.log(listing);
-  res.redirect(`/listings/${listing._id}`);
-});
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findByIdAndUpdate(req.params.id, {
+      ...req.body.listing,
+    });
+    console.log(listing);
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
-  let deletedListing = await Listing.findByIdAndDelete(req.params.id);
-  console.log(deletedListing);
-  res.redirect("/listings");
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let deletedListing = await Listing.findByIdAndDelete(req.params.id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+// app.all("*", (req, res, next) => {                 //I need to check why this is not working
+//   next(new ExpressError(404, "Page Not Found"));
+// });
+
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Something Went Wrong" } = err;
+  res.status(status).render("errors/error.ejs", { status, message });
+  // res.status(status).send(message);
 });
 
 app.listen(PORT, () => {
